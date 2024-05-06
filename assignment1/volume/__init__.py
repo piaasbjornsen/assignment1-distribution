6736e739dc67a6b1434ff4f54781debc4ecd4e62
piaasbjornsen <pia.asbjornsen@icloud.com>
2024-05-06 18:18:46 +0200

    task 1.2 returning mesh volume working

--- assignment1/volume/__init__.py
+++ assignment1/volume/__init__.py
@@ -14,16 +14,20 @@ class MeshVolume(bpy.types.Panel):
     bl_region_type = "UI"
 
     def draw(self, context):
-
         # TODO: Check that the user has selected a mesh
+        if context.active_object is None:
+            self.layout.label(text='Select a mesh')
+            return 
 
         # TODO: Obtain a BMesh from the selected mesh
+        bm = bmesh.new()
+        bm.from_mesh(context.active_object.data)
+
 
         # TODO: Apply the world transformation to the BMesh (so that scaling affects volume)
-
+        bmesh.ops.transform(bm, matrix=context.active_object.matrix_world, verts=bm.verts)
+        
         # TODO: Show the computed volume using a label
-
-        pass
-
+        self.layout.label(text=f'Volume: {mesh_volume(bm):.2f} cubic units')
 
 
