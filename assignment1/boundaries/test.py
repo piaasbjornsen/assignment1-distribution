079cf713ee43e56a20fecc7f03634cfc1a144a5e
piaasbjornsen <pia.asbjornsen@icloud.com>
2024-05-15 14:29:10 +0200

    boundary loop test, passing

--- assignment1/boundaries/test.py
+++ assignment1/boundaries/test.py
@@ -7,4 +7,34 @@ from data import primitives, meshes
 
 class TestBoundaries(unittest.TestCase):
     # HINT: add some unit tests!
-    pass
+    def test_double_toroid(self):
+        self.assertEqual(
+            len(mesh_boundary_loops(meshes.DOUBLE_TORUS)), 0, "The double toroid should have 0 boundary loops"
+        )
+
+    def test_bagel_cut_torus(self):
+        self.assertEqual(
+            len(mesh_boundary_loops(meshes.BAGEL_CUT_TORUS)),
+            4,
+            "The bagel cut torus should have 4 boundary loops ",
+        )
+
+    def test_half_bagel_cut_torus(self):
+        self.assertEqual(
+            len(mesh_boundary_loops(meshes.HALF_BAGEL_CUT_TORUS)),
+            2,
+            "The half bagel cut torus should have 2 boundary loops",
+        )
+
+    def test_half_torus(self):
+        self.assertEqual(
+            len(mesh_boundary_loops(meshes.HALF_TORUS)), 2, "The half torus should have 2 boundary loops"
+        )
+
+    def test_two_tori(self):
+        self.assertEqual(
+            len(mesh_boundary_loops(meshes.TWO_TORI)),
+            0,
+            "The two tori should have 0 boundary loops",
+        )
+
