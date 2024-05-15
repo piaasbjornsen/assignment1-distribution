9b810bc07f044f4cc9cabc3b7ae3700094278fec
piaasbjornsen <pia.asbjornsen@icloud.com>
2024-05-15 14:07:48 +0200

    test path for pia

---
+++ test_pia.sh
@@ -0,0 +1,2 @@
+# You'll probably need to adapt the following line to match your system!
+/Applications/Blender.app/Contents/MacOS/Blender --python /Users/piaasbjornsen/Documents/V2024/GEO/assignment1-distribution/test.py
