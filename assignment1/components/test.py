6fc2dbf20d7b5ed6af7330148ed93a5bd3c55834
piaasbjornsen <pia.asbjornsen@icloud.com>
2024-05-15 14:40:04 +0200

    components test, passing

--- assignment1/components/test.py
+++ assignment1/components/test.py
@@ -11,5 +11,34 @@ class TestConnectedComponents(unittest.TestCase):
     #       (You can create examples in the Blender UI by selecting multiple objects and pressing CTRl-J)
 
     # HINT: Add automated testing of larger component counts, automatically generate examples by combining primitives
+    def test_double_toroid(self):
+        self.assertEqual(
+            len(mesh_connected_components(meshes.DOUBLE_TORUS)), 1, "The double toroid should have 1 component"
+        )
 
-    pass
+    def test_bagel_cut_torus(self):
+        self.assertEqual(
+            len(mesh_connected_components(meshes.BAGEL_CUT_TORUS)),
+            2,
+            "The bagel cut torus should have 2 components",
+        )
+
+    def test_half_bagel_cut_torus(self):
+        self.assertEqual(
+            len(mesh_connected_components(meshes.HALF_BAGEL_CUT_TORUS)),
+            1,
+            "The half bagel cut torus should have 1 component",
+        )
+
+    def test_half_torus(self):
+        self.assertEqual(
+            len(mesh_connected_components(meshes.HALF_TORUS)), 1, "The half torus should have 1 component"
+        )
+
+    def test_two_tori(self):
+        self.assertEqual(
+            len(mesh_connected_components(meshes.TWO_TORI)),
+            2,
+            "The two tori should have 2 components",
+        )
+
